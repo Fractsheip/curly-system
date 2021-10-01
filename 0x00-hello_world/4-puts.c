8df02f43189d4fecd0b66a2ad4aa6a8d3f15234f
Fractsheip <fractsheip@gmail.com>
2021-09-30 20:53:53 -0500

    5 task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - execute a text when we execute char
+ * main - execute a text when we execute the file
  * Return:the function all return 0
  */
 
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ * main - execute a text when we execute the file
+ * Return:the function all return 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

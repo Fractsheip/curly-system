1ab16c535c064fb20a1128bfb0b24606684c9b1c
Fractsheip <fractsheip@gmail.com>
2021-09-30 19:18:52 -0500

    4 task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * the function main execute a text when we execute the file
+ */
+
+int main()
+{
+	char hola = "Programming is like building a multilingual puzzle\n"
+	puts(hola);
+	return 0;
+}

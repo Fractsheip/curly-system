45233bf28ff493ce676f06a78d80659b36ce9eb7
Fractsheip <fractsheip@gmail.com>
2021-09-30 21:37:21 -0500

    6 task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,16 +12,10 @@
 
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float e;
-
-printf("Size of a char: %i byte(s)\n", a);
-printf("Size of a int: %i byte(s)\n", b);
-printf("Size of a long int: %i byte(s)\n", c);
-printf("Size of a long long int: %i byte(s)\n", d);
-printf("Size of a float: %i byte(s)\n", e)
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+printf("Size of a int: %i byte(s)\n", sizeof(int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+printf("Size of a float: %i byte(s)\n", sizeof(float))
 return (0);
 }

03e095fe218819015c29784cb6a50fc54d2c905f
Fractsheip <fractsheip@gmail.com>
2021-09-30 21:28:19 -0500

    6 task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - execute a text when we execute the file
+ * char:bytes in char
+ * int:bytes in int
+ * long int:bytes in long int
+ * long long int:bytes in long long int
+ * printf:with this can you see in the console the answer
+ * Return:the function all return 0
+ */
+
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+
+printf("Size of a char: %i byte(s)\n", a);
+printf("Size of a int: %i byte(s)\n", b);
+printf("Size of a long int: %i byte(s)\n", c)
+printf("Size of a long long int: %i byte(s)\n", d)
+printf("Size of a float: %i byte(s)\n", e)
+return (0);
+}

552054a35af2e32e6ea1cda44abf7117ba8f2409
Fractsheip <fractsheip@gmail.com>
2021-10-05 12:29:08 -0500

    5 task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - execute a text when we execute the file
+*
+* Return:the function all return 0
+*/
+
+int main(void)
+{
+int numbers = 0;
+
+while (numbers < 10)
+{
+printf("%d", numbers);
+numbers++;
+}
+printf("\n");
+return (0);
+}

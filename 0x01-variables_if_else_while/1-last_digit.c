bb068bdcebd1e40113b07fbdc2a3a4397dbcc740
Fractsheip <fractsheip@gmail.com>
2021-10-05 09:17:20 -0500

    1 task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ n = rand() - RAND_MAX / 2;
 
 if (g > 5)
 {
-printf("Last digit of %i", n "is %i", g "and is greater than 5\n");
+printf("Last digit of %d", n "is %d", g "and is greater than 5\n");
 }
 else if (g == 0)
 {
-printf("Last digit of %i", n "is %i", g "and is 0\n");
+printf("Last digit of %d", n "is %d", g "and is 0\n");
 }
 else if (g < 6)
 {
-printf("Last digit of %i", n "is %i", g "and is less than 6 and not 0\n");
+printf("Last digit of %d", n "is %d", g "and is less than 6 and not 0\n");
 }
 return (0);
 }

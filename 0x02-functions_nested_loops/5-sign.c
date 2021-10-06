c8f2a83a16b1b4a02d95f649c94a748683b33604
Fractsheip <fractsheip@gmail.com>
2021-10-06 13:59:13 -0500

    5 task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_sign - print + or - or 0 at the var n
+* @n: var define number + or - or 0
+* main.h - _putchar execute the putchar no use putchar
+* Return:the function all return 0
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+, ', n);
+}
+else if (n < 0)
+{
+_putchar('-, ', n);
+}
+else
+{
+_putchar('0, ', n)
+}
+}

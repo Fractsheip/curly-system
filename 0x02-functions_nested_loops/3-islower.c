94cad4eefc8716385a037e2656996e4642617d6b
Fractsheip <fractsheip@gmail.com>
2021-10-06 13:22:01 -0500

    3 task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _islower - print 1 if c is lowercase
+* main.h - _putchar execute the putchar no use putchar
+* Return:the function all return 0
+*/
+
+int _islower(int c)
+{
+int c;
+
+if(c <= 97 && c >= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

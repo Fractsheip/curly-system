039fb4562f05e6ba8f5759ed612c73e51544aa5e
Fractsheip <fractsheip@gmail.com>
2021-10-06 13:39:26 -0500

    4 task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _islower - print 1 if c is lowercase
+* @c: var define lowercase or otherwise
+* main.h - _putchar execute the putchar no use putchar
+* Return:the function all return 0
+*/
+
+int _islower(int c)
+{
+if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

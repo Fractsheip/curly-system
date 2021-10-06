e1a3ca0d387fbd54bae749a2d1dae21c040c1d9c
Fractsheip <fractsheip@gmail.com>
2021-10-06 12:47:32 -0500

    2 task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_alphabet_x10 - print the alphabet 10 times
+* main.h - _putchar execute the putchar no use putchar
+* Return:the function all return 0
+*/
+
+void print_alphabet_x10(void)
+{
+char c;
+int i = 0;
+
+while (i < 10)
+{
+_putchar('\n');
+i++;
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+}
+_putchar('\n');
+}

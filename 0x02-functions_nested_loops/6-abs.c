70e55f1db2a53cd9a55b9d4b8828dc3b76e772b7
Fractsheip <fractsheip@gmail.com>
2021-10-06 16:35:21 -0500

    6 task

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,8 +2,8 @@
 #include "main.h"
 
 /**
-* print_sign - print + or - or 0 at the var n
-* @n: var define number + or - or 0
+* _abs - print
+* @r: var define number + or - or 0
 * main.h - _putchar execute the putchar no use putchar
 * Return:the function all return 0
 */

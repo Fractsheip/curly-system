be375ce564a62d25b9b18c234025620bcbb714d1
Fractsheip <fractsheip@gmail.com>
2021-10-06 13:40:48 -0500

    4 task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
-* _islower - print 1 if c is lowercase
+* _isalpha - print 1 if c is lowercase
 * @c: var define lowercase or otherwise
 * main.h - _putchar execute the putchar no use putchar
 * Return:the function all return 0
 */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
 {

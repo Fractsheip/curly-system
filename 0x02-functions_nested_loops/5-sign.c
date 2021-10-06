0d1eddbdc1885db6921855975b89dc094e9dfa65
Fractsheip <fractsheip@gmail.com>
2021-10-06 16:57:03 -0500

    5 task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,16 +16,16 @@ int menos = 45;
 if (n > 0)
 {
 _putchar(mas);
-return (2);
+return (1);
 }
 else if (n < 0)
 {
 _putchar(menos);
-return (9);
+return (-1);
 }
 else
 {
 _putchar('0');
-return (8);
+return (0);
 }
 }

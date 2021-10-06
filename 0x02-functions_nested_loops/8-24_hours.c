d9fef6a2d216df4ea0da568c32d169d1790f9592
Fractsheip <fractsheip@gmail.com>
2021-10-06 18:14:04 -0500

    8 task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,7 +3,6 @@
 
 /**
 * jack_bauer - print the last digit
-* 
 * main.h - _putchar execute the putchar no use putchar
 * Return:the function all return 0
 */
@@ -15,11 +14,11 @@ int h = 0, m;
 while (h < 24)
 {
 m = 0;
-while(m < 60)
+while (m < 60)
 {
 _putchar((h / 10) + '0');
 _putchar((h % 10) + '0');
-_putchar(':');+
+_putchar(':');
 _putchar((m / 10) + '0');
 _putchar((m % 10) + '0');
 _putchar('\n');

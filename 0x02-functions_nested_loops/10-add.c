798ef0a5351b8fba9558491b5b34275c6a69b3a4
Fractsheip <fractsheip@gmail.com>
2021-10-06 23:28:33 -0500

    10 task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* add - add two integers and retuns the result
+* @a: var define the first digit
+* @b: var define the second digit
+* main.h - _putchar execute the putchar no use putchar
+* Return:the function all return 0
+*/
+
+int add(int a, int b)
+{
+return (a + b);
+}

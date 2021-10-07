8c7a34b9427827d16378dd3e8e61044e4baaf8e0
Fractsheip <fractsheip@gmail.com>
2021-10-07 15:20:15 -0500

    6 task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* print_line - return multiplies var one and var two
+* @n: var define the large of the line
+* * return: 0
+*/
+
+void print_line(int n)
+{
+int n;
+int underscore = 95;
+
+while (n > 0)
+{
+_putchar(underscore);
+n--;
+}
+_putchar('\n');
+}

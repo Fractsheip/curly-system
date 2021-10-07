7cc169446d2ac7177ddda7a525a21e4e013c3e22
Fractsheip <fractsheip@gmail.com>
2021-10-06 22:05:20 -0500

    9 task

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* times_table - print the table of 9
+* main.h - _putchar execute the putchar no use putchar
+* Return:the function all return 0
+*/
+
+void times_table(void)
+{
+int a = 0, b;
+int espacio = 32;
+int coma = 44;
+
+while(a <= 9)
+{
+int b = 0;
+while(b <= 9)
+{
+int result = a * b;
+_putchar(result);
+_putchar(espacio);
+_putchar(coma);
+b++;
+}
+_putchar('\n');
+a++;
+}
+}

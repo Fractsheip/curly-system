f02ca3d6871a87d1162905fe788754fc859f2024
Fractsheip <fractsheip@gmail.com>
2021-10-06 23:34:49 -0500

    11 task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_to_98 - print the numbers should be 98
+* @n: start var
+* main.h - _putchar execute the putchar no use putchar
+* Return:the function all return 0
+*/
+
+void print_to_98(int n)
+{
+int n;
+
+while (n < 98)
+{
+_putchar(n)
+n++;
+}
+}

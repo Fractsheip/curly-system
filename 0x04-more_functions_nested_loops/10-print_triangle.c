f1b8e0631db14203bb9dc051a89e92a2777aaac2
Fractsheip <fractsheip@gmail.com>
2021-10-07 18:39:22 -0500

    10 task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_triangle - print a triange whit size define number of a and b
+* @a: var define the large
+* @b: var define the character
+*/
+
+void print_triangle(int size)
+{
+int a = 1;
+int b = 0;
+
+if (size > 0)
+{
+while (a <= size)
+{
+b = 0;
+while (b < size - a)
+{
+putchar(' ');
+b++;
+}
+while (b < size)
+{
+putchar('#');
+b++;
+}
+putchar('\n');
+a++;
+}
+}
+else
+{
+putchar('\n');
+}
+}

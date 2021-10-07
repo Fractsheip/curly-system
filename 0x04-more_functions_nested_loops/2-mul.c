276da4850b73a05e746d2f10204a39e338863410
Fractsheip <fractsheip@gmail.com>
2021-10-07 13:08:07 -0500

    2 task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* mul - return multiplies var one and var two
+* @a: var one
+* @b: var two
+* return: return the multiplies
+*/
+
+int mul(int a, int b)
+{
+int result = a * b;
+
+return(result);
+}

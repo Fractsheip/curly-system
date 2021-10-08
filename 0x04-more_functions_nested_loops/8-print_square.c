6467eee1268c39e8f4169c9f4919d6762e1ed9fb
Fractsheip <fractsheip@gmail.com>
2021-10-08 16:24:57 -0500

    8 task

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,33 +4,31 @@
 /**
 * print_square - retun a rectangle whit equal size and large
 * @size: tell the size and large
-* @large: define the large of rectangle
-* @fijo: save the start value of size
 * return: the result
 */
 
 void print_square(int size)
 {
-    int fijo = size;
-    int large;
-    
-    if (size > 0)
-    {
-        while(size > 0)
-        {
-            large = fijo;
-            
-            while (large > 0)
-            {
-                _putchar('#');
-                large--;
-            }
-        _putchar('\n');
-        size--;
-        }
-    }
-    else
-    {
-        _putchar('\n');
-    }
+int fijo = size;
+int large;
+
+if (size > 0)
+{
+while (size > 0)
+{
+large = fijo;
+
+while (large > 0)
+{
+_putchar('#');
+large--;
+}
+_putchar('\n');
+size--;
+}
+}
+else
+{
+_putchar('\n');
+}
 }

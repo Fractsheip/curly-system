3d3795fb51d52393ed222597e33af5ce4f11117c
Fractsheip <fractsheip@gmail.com>
2021-10-08 12:52:19 -0500

    0 task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,13 +4,11 @@
 /**
 * _isupper - return if c is upercase or otherwise
 * @c: var define to evaluate
-* return: if are upercase return 1, else is otherwise return 0
+* Return:if are upercase return 1, else is otherwise return 0
 */
 
 int _isupper(int c)
 {
-int c;
-
 if (c >= 65 && c <= 90)
 {
 return (1);

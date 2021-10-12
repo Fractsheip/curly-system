25e25d531bd1bc4306893532b7aa8fca666b263c
Fractsheip <fractsheip@gmail.com>
2021-10-12 15:28:56 -0500

    9 task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*
+*
+*/
+
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] == '\0')
+	{
+		while (a != 0)
+		{
+			_putchar(s[a]);
+			a--;
+		}
+		putchar ('\n');
+	}
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strcpy - print the *src in *dest
+* @src: here are the scrpt
+* @dest: destine of the src script
+* Return: None
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+}

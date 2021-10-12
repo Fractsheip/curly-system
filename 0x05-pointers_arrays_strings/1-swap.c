a5015fb6cf4e00a127645f224f8147fc18e1fda2
Fractsheip <fractsheip@gmail.com>
2021-10-11 19:19:35 -0500

    1 task

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,12 +2,14 @@
 #include "main.h"
 
 /**
-*
-*
+* swap_int - change the values of *a, *b reverse
+* @a: var punter one
+* @b: var punter two
+* Return: None
 */
 
 void swap_int(int *a, int *b)
 {
-    *a = *b;
-    *b = *a;
+	*a = *b;
+	*b = *a;
 }

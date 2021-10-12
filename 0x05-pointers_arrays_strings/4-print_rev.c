e036526348d20df65edac655bec284cbf6ef6a07
Fractsheip <fractsheip@gmail.com>
2021-10-12 16:11:09 -0500

    4 task

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,13 @@ void print_rev(char *s)
 {
 	int a = 0;
 
+	while (s[a] != '\0')
+	{
+		a++;
+	}
 	while (s[a] == '\0')
 	{
-		while (a != 0)
+		while (a != 0 || a == 0)
 		{
 			_putchar(s[a]);
 			a--;

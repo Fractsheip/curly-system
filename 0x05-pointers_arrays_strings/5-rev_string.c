e5cf27c9fe2dc476071ff781043d40143699f826
Fractsheip <fractsheip@gmail.com>
2021-10-12 21:19:58 -0500

    5 task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,15 +13,13 @@ void rev_string(char *s)
 
 	while (s[a] != '\0')
 	{
-		_putchar(s[a]);
 		a++;
 	}
 	a--;
 	while (a >= 0)
 	{
-		_putchar(s[a]);
+		return (s[a]);
 		a--;
 	}
 	_putchar('\n');
-	
 }
